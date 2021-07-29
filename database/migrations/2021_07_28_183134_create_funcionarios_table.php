bf41284737a5170faf6cdcbaa8f1071edadec7b8
AbisaiSan <abisaiweb7@gmail.com>
2021-07-29 16:40:44 -0300

    Crud e dashboard completo

---
+++ database/migrations/2021_07_28_183134_create_funcionarios_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFuncionariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('funcionarios', function (Blueprint $table) {
+            $table->id();
+            $table->string('last_name');
+            $table->string('first_name');
+            $table->string('middle_name')->nullable();
+            $table->string('address');
+            $table->foreignId('departamento_id')->constrained();
+            $table->foreignId('pais_id')->constrained();
+            $table->foreignId('estado_id')->constrained();
+            $table->foreignId('cidade_id')->constrained();
+            $table->char('zip_code');
+            $table->date('aniversario')->nullable();
+            $table->date('data_contratacao')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('funcionarios');
+    }
+}
